0922b23c17a23d1910a92644fd2eb2afbad8c79f
Ramakoti <122818522+bandlakoti29@users.noreply.github.com>
2026-04-14 22:11:45 +0530

    Update transformations.py

--- transformations.py
+++ transformations.py
@@ -1,40 +1,31 @@
 from pyspark.sql.functions import col, when, avg, sum as _sum
 
-# -----------------------------
 # 1. Data Cleaning
-# -----------------------------
 def clean_data(df):
 
-    # Fill missing Item_Weight with average
     avg_weight = df.select(avg(col("Item_Weight"))).collect()[0][0]
     df = df.fillna({"Item_Weight": avg_weight})
 
-    # Normalize Item_Fat_Content
     df = df.withColumn(
         "Item_Fat_Content",
         when(col("Item_Fat_Content").isin("LF", "low fat"), "Low Fat")
         .when(col("Item_Fat_Content") == "reg", "Regular")
         .otherwise(col("Item_Fat_Content"))
     )
-
-    # Fill Outlet_Size missing values
+    
     df = df.fillna({"Outlet_Size": "Medium"})
 
     return df
 
 
-# -----------------------------
 # 2. Feature Engineering
-# -----------------------------
 def feature_engineering(df):
 
-    # Outlet Age
     df = df.withColumn(
         "Outlet_Age",
         2026 - col("Outlet_Establishment_Year")
     )
 
-    # Price Category
     df = df.withColumn(
         "Price_Category",
         when(col("Item_MRP") < 70, "Low")
@@ -45,9 +36,8 @@ def feature_engineering(df):
     return df
 
 
-# -----------------------------
-# 3. Aggregations (Business Insights)
-# -----------------------------
+
+# 3. Aggregations
 def aggregate_data(df):
 
     df_agg = df.groupBy("Outlet_Type").agg(
